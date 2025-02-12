5531d60254e8187b143f2e7cde1914a0f3eaffd7
aalfath7 <abdielhafizhy@gmail.com>
2025-02-12 17:14:27 +0700

    edit navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <nav></nav>
-      <div>Welcome</div>
+      <nav className="flex justify-between py-4 px-10 shadow">
+        <Link href="/">Alfath</Link>
+        <div className="space-x-5">
+          <Link href="/about">About</Link>
+          <Link href="/contact">contact</Link>
+        </div>
+      </nav>
+      <div className="p-10">Welcome</div>
     </div>
   );
 }
