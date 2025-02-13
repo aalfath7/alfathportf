734ec18399d341e452932098346e8d9fa2ab85db
aalfath7 <abdielhafizhy@gmail.com>
2025-02-13 08:29:37 +0700

    add layout folder

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,6 @@
-import Image from "next/image";
-import Link from "next/link";
-
 export default function Home() {
   return (
     <div>
-      <nav className="flex justify-between py-4 px-10 shadow">
-        <Link href="/">Alfath</Link>
-        <div className="space-x-5">
-          <Link href="/about">About</Link>
-          <Link href="/contact">contact</Link>
-        </div>
-      </nav>
       <div className="p-10">Welcome</div>
     </div>
   );
