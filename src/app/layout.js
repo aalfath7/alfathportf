7a03cd28933dcfd92bb53f30d93f967538ebb577
aalfath7 <abdielhafizhy@gmail.com>
2025-02-21 05:33:59 +0700

    compress image, change font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,20 @@
 import Link from "next/link";
+import Image from "next/image";
 import "../styles/globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
       <body className="relative h-screen w-full">
-        <div className="absolute inset-0 bg-[url('/images/bg.png')] bg-cover bg-center"></div>
+        <Image
+          src="/images/bg.png"
+          alt=""
+          width={500}
+          height={600}
+          className="absolute w-full h-screen object-cover"
+          quality={50}
+        />
+        {/* <div className="absolute inset-0 bg-[url('/images/bg.png')] bg-cover bg-center"></div> */}
         <div className="absolute inset-0 bg-black/50 backdrop-blur-sm"></div>
 
         <nav className="glass relative z-10 bg-white flex justify-between py-4 px-10 shadow">
